b9bccdeab074dab51bc9f34d1406793477da93c9
AndreasDEV <andreasmrdja16@gmail.com>
2019-02-09 00:21:31 +0100

    Added image zoom on click. RFI. | Update #9 |

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,2 +1,22 @@
 // * --- WEBPACK IMPORT FILES --- * \\
 import '../sass/main.scss';
+
+// -- MODAL IMAGES -- \\
+
+// DOM Elements
+const modal = document.querySelector('#modal');
+const image = [...document.querySelectorAll('.img-o')]; // converting node-list to array
+const modalImg = [...document.querySelectorAll('.img-m')];
+const span = document.querySelector('.close');
+
+image.forEach(el => {
+    // Event Listener | Open image on click
+    el.addEventListener('click', () => {
+    modal.style.display = 'block';
+    modalImg.src = el.src;
+});
+})
+
+span.addEventListener('click', () => {
+    modal.style.display = 'none';
+});

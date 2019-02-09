96c4cb6fc96478e00121dcefde138d72685b7c8c
AndreasDEV <andreasmrdja16@gmail.com>
2019-02-09 18:11:38 +0100

    Improved style and JS code for image view in gallery section. | Update #10 |

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,22 +1,25 @@
 // * --- WEBPACK IMPORT FILES --- * \\
 import '../sass/main.scss';
 
+
+
 // -- MODAL IMAGES -- \\
+(() => { // IIFE
+    // DOM Elements
+    const modal = document.querySelector('#modal');
+    const image = [...document.querySelectorAll('.img-o')]; // converting node-list to array
+    const modalImg = [...document.querySelectorAll('.img-m')]; // -||-
+    const closeBtn = document.querySelector('.close-container');
+    
+    // - EVENT LISTENERS - | - For each image add click event (image view)
+    image.forEach(el => {
+            // Event Listener | Open image on click
+            el.addEventListener('click', () => {
+            modal.style.display = 'block';
+            modalImg.src = el.src;
+        });
+    });
 
-// DOM Elements
-const modal = document.querySelector('#modal');
-const image = [...document.querySelectorAll('.img-o')]; // converting node-list to array
-const modalImg = [...document.querySelectorAll('.img-m')];
-const span = document.querySelector('.close');
-
-image.forEach(el => {
-    // Event Listener | Open image on click
-    el.addEventListener('click', () => {
-    modal.style.display = 'block';
-    modalImg.src = el.src;
-});
-})
-
-span.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
+    // - EVENT LISTENER - | - Close the image view on button click
+    closeBtn.addEventListener('click', () => { modal.style.display = 'none'; });
+})();
